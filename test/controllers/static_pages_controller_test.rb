4783abb10f86a5d52ed996550425f60a7827f4d8
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-01-18 22:07:04 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -16,4 +21,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
 end
