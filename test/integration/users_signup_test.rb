91dd74e0fe5bee4931bf18e6d309c78bbf98dda6
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-01-31 23:58:29 +0900

    Rails7 7.5.0

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
     assert_template 'users/new'
   end
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.（コードを書き込む）
+  end
 end
