332a7bcca537466b85b42580eb4b97e853de429c
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-01 00:45:10 +0900

    Use SSL and the Puma web server in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.（コードを書き込む）
+    assert_not flash. empty?
   end
 end
