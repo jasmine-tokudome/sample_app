d7aad56afb7234d31eb2f0fd11ca088f359e3466
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-01 23:51:08 +0900

    Rails7 8.1.4

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      # エラーメッセージを作成する
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
