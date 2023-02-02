c19ef9d3241cbd0cd7927ea30b19b9593646422c
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-03 00:39:16 +0900

    Rails7 8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new', status: :unprocessable_entity˜˜
+      render 'new', status: :unprocessable_entity
     end
   end
 
