02ae96461092417ad15a0d1f06f553fdd42fd6c4
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-03 23:59:24 +0900

    Rails7.8.2.6

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
