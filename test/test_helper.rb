b6d29b48aa48767eabfae12d816bc2bbda67f4fd
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-04 23:52:10 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  class ActiveSupport::TestCase
+    # 指定のワーカー数でテストを並列実行する
+    parallelize(workers: :number_of_processors)
+    # test/fixtures/*.ymlのfixtureをすべてセットアップする
+    fixtures :all
+  
+    # テストユーザーがログイン中の場合にtrueを返す
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+  end  
 end
