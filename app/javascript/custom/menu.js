23f0199ed81dbdf6759dd51da344da1b4ead161f
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-07 23:19:10 +0900

    Rails7 10.1.1

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,17 +1,18 @@
 // メニュー操作
 
-// トグルリスナーを追加する
-function addToggleListener(selected_id, menu_id, toggle_class) {
-    let selected_element = document.querySelector(`#${selected_id}`);
-    selected_element.addEventListener("click", function (event) {
-        event.preventDefault();
-        let menu = document.querySelector(`#${menu_id}`);
-        menu.classList.toggle(toggle_class);
-    });
-}
-
-// クリックをリッスンするトグルリスナーを追加する
+// トグルリスナーを追加してクリックをリッスンする
 document.addEventListener("turbo:load", function () {
-    addToggleListener("hamburger", "navbar-menu", "collapse");
-    addToggleListener("account", "dropdown-menu", "active");
+    let hamburger = document.querySelector("#hamburger");
+    hamburger.addEventListener("click", function (event) {
+        event.preventDefault();
+        let menu = document.querySelector("#navbar-menu");
+        menu.classList.toggle("collapse");
+    });
+
+    let account = document.querySelector("#account");
+    account.addEventListener("click", function (event) {
+        event.preventDefault();
+        let menu = document.querySelector("#dropdown-menu");
+        menu.classList.toggle("active");
+    });
 });
