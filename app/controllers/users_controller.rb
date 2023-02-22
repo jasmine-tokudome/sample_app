c80ad0f5ac15ec8cc5888e971aef0f18eb1d91e3
jasmine-tokudome <orange_blossom_1684mk@yahoo.co.jp>
2023-02-22 23:07:38 +0900

    Rails7 11.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
--- db/migrate/20230122150428_add_index_to_users_email.rb
+++ db/migrate/20230122150428_add_index_to_users_email.rb
@@ -1,4 +1,4 @@
-\class AddIndexToUsersEmail < ActiveRecord::Migration[7.0]
+class AddIndexToUsersEmail < ActiveRecord::Migration[7.0]
 def change
   add_index :users, :email, unique: true
 end
