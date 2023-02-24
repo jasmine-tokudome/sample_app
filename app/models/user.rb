45e4c8a7ccaaa2257b196771faea6bfd6c8f32aa
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-24 19:14:47 +0900

    Rails7 12.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
     attr_accessor :remember_token, :activation_token
     before_save   :downcase_email
     before_create :create_activation_digest
@@ -57,6 +58,18 @@ class User < ApplicationRecord
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
+
+    # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
   
     private
   
