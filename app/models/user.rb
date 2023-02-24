2668dbf145001a9dd72736a6e1a7d0eaf67817b3
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-25 01:45:34 +0900

    Rails7 12.3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,11 @@ class User < ApplicationRecord
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago   #< 記号は「〜より早い時刻」と読む
+  end
   
     private
   
