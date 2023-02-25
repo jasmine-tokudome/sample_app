48246444943edc7ea0773ea5990e6ecaf04ad29a
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 00:10:38 +0900

    Rails7 13.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
     attr_accessor :remember_token, :activation_token
     before_save   :downcase_email
