f4547d1499a03bb6c88cc3b721920c2c4c34f7d8
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 02:32:55 +0900

    Rails7 13.2.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,14 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password
+               password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
-  activated: true,
-    activated_at: Time.zone.now)
+end
+
+  # ユーザーの一部を対象にマイクロポストを生成する
+  users = User.order(:created_at).take(6)
+  50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
