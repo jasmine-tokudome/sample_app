0b41da317426af41cfed50085e08ec68d02fe01b
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 18:45:37 +0900

    Rails7 14.2.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# メインのサンプルユーザーを1人作成する
+# Users
 User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
@@ -7,7 +7,6 @@ User.create!(name:  "Example User",
   activated: true,
   activated_at: Time.zone.now)
 
-# 追加のユーザーをまとめて生成する
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -20,9 +19,17 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
-  # ユーザーの一部を対象にマイクロポストを生成する
+  #マイクロポスト
   users = User.order(:created_at).take(6)
   50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
