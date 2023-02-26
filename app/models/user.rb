e1047ff9df1411317d75bab38b86344b9b53cd85
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 20:10:19 +0900

    Rails7 14.2.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,11 +84,10 @@ class User < ApplicationRecord
      reset_sent_at < 2.hours.ago   #< 記号は「〜より早い時刻」と読む
    end
 
-   # 試作feedの定義
-   # 完全な実装は次章の「ユーザーをフォローする」を参照
-   def feed
-     Micropost.where("user_id = ?", id)
-   end
+   # ユーザーのステータスフィードを返す
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
 
    # ユーザーをフォローする
    def follow(other_user)
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
