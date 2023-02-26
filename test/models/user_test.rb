8ca7cff41439cdeeed2ff6c3f2940e903d871e70
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 21:00:44 +0900

    Rails7 14.3.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,4 +91,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(michael)
   end
 
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # フォローしているユーザーの投稿を確認
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # フォロワーがいるユーザー自身の投稿を確認
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # フォローしていないユーザーの投稿を確認
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
