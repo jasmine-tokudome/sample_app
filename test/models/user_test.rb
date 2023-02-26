ed6b6fb090ad78e45a832687ec6dbe0b6cbe52bc
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 18:40:00 +0900

    Rails7 14.1.5

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,11 +83,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
     # ユーザーは自分自身をフォローできない
     michael.follow(michael)
     assert_not michael.following?(michael)
   end
-  
+
 end
