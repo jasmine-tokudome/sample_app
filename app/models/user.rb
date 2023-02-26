8884c8624ea7716f35c4ddb4ee8128ef48526a48
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-02-26 21:41:21 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,7 +86,11 @@ class User < ApplicationRecord
 
    # ユーザーのステータスフィードを返す
    def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+   following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+             .includes(:user, image_attachment: :blob)
    end
 
    # ユーザーをフォローする
